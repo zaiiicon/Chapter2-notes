3a83577397f2264e628395cc923535032a0ad657
8t8f <8t8f@fresnou.org>
2023-02-07 14:16:22 -0800

    initial commit

---
+++ .idea/Conditionals.js
@@ -0,0 +1,41 @@
+//Conditionals are if-statements
+
+
+let x=5;
+if (x==5){
+    console.log("it's 5!");
+}
+console.log("end of program");
+
+
+let y =10;
+if(y==5){
+    console.log("it's 5!");
+}
+console.log("end of program");
+
+
+let z = 20;
+if (z==5){
+    console.log("option1");
+} else {
+    console.log("option2");
+}
+console.log("end of program");
+
+
+let w =20;
+if (w==20){
+    console.log("option1");
+} else {
+console.log("option2");
+}
+console.log("end of program");
+
+
+let num1=100;
+if (num1==10){
+
+}else if (num1==6){
+
+}
